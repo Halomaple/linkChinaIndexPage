262a626c135e9f680e4fe3daa308b19ca150391a
Jianfeng <jeff.doyle@foxmail.com>
2017-03-13 21:04:38 +0800

    Second version

--- js/linkchina-index.js
+++ js/linkchina-index.js
@@ -1,3 +1,88 @@
+//DNA animation
+var animDelay = ('animationDelay' in document.documentElement.style) ? 'animationDelay' : 'webkitAnimationDelay';
+
+(function(window, document) {
+	// var count = Math.ceil(window.innerWidth / 12 * 2);
+	var count = 50,
+		i = 0,
+		div, delay;
+	var divList = document.createDocumentFragment();
+	for (i; i < count; i++) {
+		div = document.createElement('div');
+		delay = (i - count) * 0.2;
+		div.style[animDelay] = delay + 's,' + delay / 2 + 's';
+		divList.appendChild(div);
+	}
+	document.getElementById('container').appendChild(divList);
+})(window, document);
+
+(function(window, document) {
+	// var count = Math.ceil(window.innerWidth / 12 * 2);
+	var count = 10,
+		i = 0,
+		div, delay;
+	var divList = document.createDocumentFragment();
+	for (i; i < count; i++) {
+		div = document.createElement('div');
+		delay = (i - count) * 0.2;
+		div.style[animDelay] = delay + 's,' + delay / 2 + 's';
+		divList.appendChild(div);
+	}
+	document.getElementById('container2').appendChild(divList);
+})(window, document);
+
+(function(window, document) {
+	// var count = Math.ceil(window.innerWidth / 12 * 2);
+	var count = 25,
+		i = 0,
+		div, delay;
+	var divList = document.createDocumentFragment();
+	for (i; i < count; i++) {
+		div = document.createElement('div');
+		delay = (i - count) * 0.2;
+		div.style[animDelay] = delay + 's,' + delay / 2 + 's';
+		divList.appendChild(div);
+	}
+	document.getElementById('container3').appendChild(divList);
+})(window, document);
+
+(function(window, document) {
+	// var count = Math.ceil(window.innerWidth / 12 * 2);
+	var count = 65,
+		i = 0,
+		div, delay;
+	var divList = document.createDocumentFragment();
+	for (i; i < count; i++) {
+		div = document.createElement('div');
+		delay = (i - count) * 0.2;
+		div.style[animDelay] = delay + 's,' + delay / 2 + 's';
+		divList.appendChild(div);
+	}
+	document.getElementById('container4').appendChild(divList);
+})(window, document);
+
+//Dot zooming
+(function(window, document) {
+	var i = 0,
+		count = 20,
+		div;
+	for (i; i < count; i++) {
+		div = document.createElement('div');
+		delay = (i - count) * 0.6;
+		var radius = parseInt(Math.random() * (15) + 15, 10) + 'px';
+
+		div.style.width = radius;
+		div.style.height = radius;
+
+		div.style.left = parseInt(Math.random() * (window.innerWidth - 10), 10) + 'px';
+		div.style.top = parseInt(Math.random() * (window.innerHeight - 10), 10) + 'px';
+
+		div.style[animDelay] = delay + 's,' + delay / 2 + 's';
+
+		document.getElementById('zooming-dot').appendChild(div);
+	}
+})(window, document);
+
 $(document).ready(function() {
 	$('.inputmark').val(0);
 	$('.inputmark2').val(0);
